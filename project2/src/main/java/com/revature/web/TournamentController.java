cfc43277bda21cac1c543cf6fcf9263e6381eed9
synewaz <32374786+synewaz@users.noreply.github.com>
2019-07-19 09:16:16 -0500

    new change

---
+++ project2/src/main/java/com/revature/web/TournamentController.java
@@ -0,0 +1,11 @@
+package com.revature.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/tournament")
+public class TournamentController {
+
+	
+}
