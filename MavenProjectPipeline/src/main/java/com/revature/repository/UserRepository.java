edf6322026d5553730098430cd2f481687122bfd
synewaz <32374786+synewaz@users.noreply.github.com>
2019-07-21 19:57:16 -0500

    mvc capabilities added

--- MavenProjectPipeline/src/main/java/com/revature/repository/UserRepository.java
+++ MavenProjectPipeline/src/main/java/com/revature/repository/UserRepository.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Repository;
 
 import com.revature.model.User;
 import com.revature.util.SessionFactory;
+
+import com.revature.repository.UserRepository;
 
 @Repository("ur")
 public class UserRepository {
@@ -87,4 +92,28 @@ public class UserRepository {
 		
 		return u;
 	}
+
+	public boolean userLogin(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+//		Session s = null;
+		System.out.println("We are in the repository layer of the log in method.");
+		final String username = req.getParameter("username");
+		final String password = req.getParameter("password");
+		
+		if (username != null && !username.equals("") && password != null && !password.equals("")) {
+			UserRepository us = new UserRepository();
+			if (us.getUserByUsername(username) != null
+					&& password.equals(us.getUserByUsername(username).getPassword())) {
+				
+				HttpSession s = req.getSession();
+				
+				s.setAttribute("username", username);
+				s.setAttribute("user_id", us.getUserByUsername(username).getUser_id());
+
+				return true;
+			}
+	}
+		return false;
+
 }
+	}
--- MavenProjectPipeline/src/main/java/com/revature/service/UserService.java
+++ MavenProjectPipeline/src/main/java/com/revature/service/UserService.java
@@ -2,6 +2,9 @@ package com.revature.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,4 +31,10 @@ private UserRepository ur;
     public User getUserById(int user_id) {
     	return ur.getUserById(user_id);
     }
+
+	public boolean userLogin(HttpServletRequest req, HttpServletResponse resp) {
+		System.out.println("we are in the service layer of the log in method.");
+	return	ur.userLogin(req, resp);
+		
+	}
 }
