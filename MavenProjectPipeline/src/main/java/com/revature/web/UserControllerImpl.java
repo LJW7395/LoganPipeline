f4f3cff9c678ba3ff00bf995a44a0a6aaf5d38ce
unknown <loganjwillis@gmail.com>
2019-07-25 10:14:03 -0500

    adding work done Wednesday night

--- MavenProjectPipeline/src/main/java/com/revature/web/UserControllerImpl.java
+++ MavenProjectPipeline/src/main/java/com/revature/web/UserControllerImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,11 +18,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.revature.model.User;
 import com.revature.service.UserService;
@@ -75,13 +76,13 @@ public class UserControllerImpl {
 		System.out.println(httpServletRequestToString(req));
 		User user = us.userLogin(u, req);
 		if (user != null) {
-			//userInfo(u);
+			// userInfo(u);
 			return new ResponseEntity<Object>(user, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<Object>(u, HttpStatus.UNAUTHORIZED);
 		}
 	}
-	
+
 //	@GetMapping(value = "/info")
 //	public String userInfo(@SessionAttribute("u") User u) {
 //		System.out.println("username is: " + u.getUsername());
@@ -106,47 +107,59 @@ public class UserControllerImpl {
 //		return null;
 //	}
 
-	@PostMapping(value = "/registration", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseEntity<Object> postUser(@RequestBody User u) {
-       
-        System.out.println("The movie we received is " + u);
-		 us.userRegistration(u);;
-		 if (u != null) {
-				//userInfo(u);
-				return new ResponseEntity<Object>(u, HttpStatus.OK);
-			} else {
-				return new ResponseEntity<Object>(u, HttpStatus.UNAUTHORIZED);
-			}
-    }
-	
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public @ResponseBody User postUser(@RequestParam(name = "username") String username,
+			@RequestParam(name = "password") String password, @RequestParam(name = "first_name") String first_name,
+			@RequestParam(name = "last_name") String last_name, @RequestParam(name = "region") String region,
+			@RequestParam(name = "phrase") String phrase, @RequestParam(name = "users_role") String users_role,
+			HttpServletResponse resp) throws IOException {
+		User u = new User();
+		u.setUsername(username);
+		u.setPassword(password);
+		u.setFirst_name(first_name);
+		u.setLast_name(last_name);
+		u.setRegion(region);
+		u.setRanking(50);
+		u.setPhrase(phrase);
+		u.setPoints(1000);
+		u.setUsers_role(users_role);
+		System.out.println("The movie we received is " + u);
+		us.userRegistration(u);
+		if (u != null) {
+			// userInfo(u);
+			resp.sendRedirect("localhost:4200/login");
+			return u;
+		} else {
+			return u;
+		}
+	}
+
 	private String httpServletRequestToString(HttpServletRequest request) {
-	    StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 
-	    sb.append("Request Method = [" + request.getMethod() + "], ");
-	    sb.append("Request URL Path = [" + request.getRequestURL() + "], ");
+		sb.append("Request Method = [" + request.getMethod() + "], ");
+		sb.append("Request URL Path = [" + request.getRequestURL() + "], ");
 
-	    String headers =
-	        (String) Collections.list(request.getHeaderNames()).stream()
-		    .map(headerName -> headerName + " : " + Collections.list(request.getHeaders((String) headerName)) )
-		    .collect(Collectors.joining(", "));
+		String headers = (String) Collections.list(request.getHeaderNames()).stream()
+				.map(headerName -> headerName + " : " + Collections.list(request.getHeaders((String) headerName)))
+				.collect(Collectors.joining(", "));
 
-	    if (headers.isEmpty()) {
-	        sb.append("Request headers: NONE,");
-	    } else {
-	        sb.append("Request headers: ["+headers+"],");
-	    }
+		if (headers.isEmpty()) {
+			sb.append("Request headers: NONE,");
+		} else {
+			sb.append("Request headers: [" + headers + "],");
+		}
 
-	    String parameters =
-	        (String) Collections.list(request.getParameterNames()).stream()
-		    .map(p -> p + " : " + Arrays.asList( request.getParameterValues((String) p)) )
-		    .collect(Collectors.joining(", "));             
+		String parameters = (String) Collections.list(request.getParameterNames()).stream()
+				.map(p -> p + " : " + Arrays.asList(request.getParameterValues((String) p)))
+				.collect(Collectors.joining(", "));
 
-	    if (parameters.isEmpty()) {
-	        sb.append("Request parameters: NONE.");
-	    } else {
-	        sb.append("Request parameters: [" + parameters + "].");
-	    }
+		if (parameters.isEmpty()) {
+			sb.append("Request parameters: NONE.");
+		} else {
+			sb.append("Request parameters: [" + parameters + "].");
+		}
 
-	    return sb.toString();
+		return sb.toString();
 	}
 }
